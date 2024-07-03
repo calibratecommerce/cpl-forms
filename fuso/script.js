82babfe5737254d57868357f8013d36b3f247929
developers-calibrate <152395904+developers-calibrate@users.noreply.github.com>
2024-07-03 15:46:40 +0400

    Update script.js

--- fuso/script.js
+++ fuso/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-            const cheryForm = document.getElementById("cheryForm");
+            const fusoForm = document.getElementById("fusoForm");
 
-            cheryForm.addEventListener("submit", function(e) {
+            fusoForm.addEventListener("submit", function(e) {
                 e.preventDefault();
                 // Send the form data to the Google Apps Script web app
-                const formData = new FormData(cheryForm);
-                fetch(cheryForm.action, {
+                const formData = new FormData(fusoForm);
+                fetch(fusoForm.action, {
                     method: 'POST',
                     body: formData,
                 })
                 .then(response => response.text())
                 .then(responseText => {
                     console.log(responseText);
-                    cheryForm.style.display = "none";
+                    fusoForm.style.display = "none";
                     document.getElementById("thankYouMessage").style.display = "block";
                 })
                 .catch(error => console.error('Error:', error));
